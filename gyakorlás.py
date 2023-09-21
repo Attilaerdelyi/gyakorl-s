0c1f40629544bd2bfbe6a42d4faaaf743e3b7354
Attilaerdelyi <145645524+Attilaerdelyi@users.noreply.github.com>
2023-09-21 08:20:07 +0200

    xdxd
    xdxd

---
+++ gyakorlás.py
@@ -0,0 +1,63 @@
+#1 feladat kérjünk be a felhasználótól kettõ darab egész számot, és vizsgáljuk meg a következõket, elsõként vizsgálja meg hogy melyik a nagyobb érték, illetve a két érték esetlegesen egyenlõ-e
+szam1 = int(input ("Adjon meg egy számot:"))
+szam2 = int(input ("Adjon meg egy számot:"))
+
+if szam1 > szam2:
+    print("Az elsõ szám nagyobb mint a második")
+
+if szam1 < szam2:
+    print ("A második szám nagyobb mint az elsõ")
+    
+if szam1 == szam2:
+    print ("A megadott értékek egyenlõek")
+
+
+
+
+
+#2 feladatbekérünk a felhasználótól 5db egész számot, mindegyiket megvizsgáljuk hogy pozitív vagy negatív-e, és azt is hogy nagyobb e mint 20
+a = int(input("adjon meg egy számot:"))
+if a > 0:
+    print("A szám pozitív")
+    
+if a < 0:
+    print("A szám negatív")
+if a > 20:
+    print("A szám nagyobb mint 20")
+if a < 20:
+    print("A szám kisebb mint 20")
+    
+b = int(input("adjon meg egy számot:"))
+if b > 0:
+    print("A szám pozitív")
+    
+if b < 0:
+    print("A szám negatív")
+if b > 20:
+    print("A szám nagyobb mint 20")
+if b < 20:
+    print("A szám kisebb mint 20")
+d = int(input("adjon meg egy számot:"))
+if d > 0:
+    print("A szám pozitív")
+    
+if d < 0:
+    print("A szám negatív")
+if d > 20:
+    print("A szám nagyobb mint 20")
+if d < 20:
+    print("A szám kisebb mint 20")
+e = int(input("adjon meg egy számot:"))
+if a > 0:
+    print("A szám pozitív")
+    
+if e < 0:
+    print("A szám negatív")
+if e > 20:
+    print("A szám nagyobb mint 20")
+if e < 20:
+    print("A szám kisebb mint 20")
+    
+
+
+#+1 hány olyan van az 5 közül ami pozitív és 20-nál nagyobb is.
